cde2f06b79f637d8e7996dd45b0953084abc69f4
anth <eldiegoemanuel@gmail.com>
2025-09-08 11:40:21 -0500

    feat use italics and tokyonight how main theme

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,1 +1,7 @@
-vim.cmd.colorscheme("retrobox")
+vim.cmd.colorscheme("tokyonight")
+vim.cmd [[
+  highlight Comment gui=italic cterm=italic
+  highlight Keyword gui=italic cterm=italic
+  highlight Conditional gui=italic cterm=italic
+  highlight Type gui=italic cterm=italic
+]]

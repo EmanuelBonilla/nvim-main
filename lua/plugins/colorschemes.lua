52153284f1d3830e455cce05c0faa0f2a3fe3bb3
anth <eldiegoemanuel@gmail.com>
2025-09-10 11:49:50 -0500

    feat colorschemes now use italics forever and canchange with uc

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -10,5 +10,31 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = false,
-  }
+    config = function()
+      local function next_colorscheme()
+        local select_colorschemes = { "tokyonight", "tokyonight-moon", "dark_plus", "VisualStudioDark", "retrobox" }
+        local current = vim.g.colors_name
+
+        for i, cs in ipairs(select_colorschemes) do
+          if cs == current then
+            local next = select_colorschemes[i + 1] or select_colorschemes[1]
+            vim.cmd.colorscheme(next)
+            return
+          end
+        end
+        vim.notify("Current colorscheme: " .. vim.g.colors_name)
+      end
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = function()
+          vim.cmd [[
+            highlight Comment gui=italic cterm=italic
+            highlight Keyword gui=italic cterm=italic
+            highlight Conditional gui=italic cterm=italic
+            highlight Type gui=italic cterm=italic
+          ]]
+        end,
+      })
+      vim.keymap.set("n", "<leader>uc", next_colorscheme, { desc = "Change colorscheme" })
+    end,
+  },
 }

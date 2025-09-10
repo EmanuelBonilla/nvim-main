527b5f6720c494d4f13a78184614aebd53eca9f2
anth <eldiegoemanuel@gmail.com>
2025-09-10 11:49:32 -0500

    feat set commando for on or of transparence

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,5 +1,8 @@
 return {
   "https://github.com/xiyaowong/transparent.nvim",
   lazy = false,
-  priority = 1000
+  priority = 1000,
+  config = function()
+    vim.keymap.set("n", "<leader>ut", ":TransparentToggle<CR>", { desc = "Toggle transparency" })
+  end
 }

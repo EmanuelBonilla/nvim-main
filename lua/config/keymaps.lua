5e6e2e797c11bfcaabc13f37f10994e1e9b08ddc
anth <eldiegoemanuel@gmail.com>
2025-09-10 12:10:36 -0500

    ref set english all descriptions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,12 +2,13 @@
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { noremap = true, silent = true })
 vim.keymap.set({ 'n', 'i' }, '<C-s>', ':w<CR>', { desc = "Save File", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>q", ":qa<CR>", { desc = "Close Nvim" })
+
 -- Windows
 vim.keymap.set("n", "<leader>w", "<C-w>", { desc = "Use windows commands" })
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Ir a la ventana izquierda" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Ir a la ventana derecha" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Ir a la ventana abajo" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Ir a la ventana arriba" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to down window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to up window" })
 
 -- Buffers
 vim.keymap.set("n", "<leader>bd", ":bd<CR>", { desc = "Close Current Buffer" })

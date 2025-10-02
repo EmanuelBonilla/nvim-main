77ce22e5c490106340e202c7ca7e7867e1a7ad23
anth <eldiegoemanuel@gmail.com>
2025-10-01 19:46:10 -0500

    feat use environemnt variables for define colorscheme and transparency

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,5 +1,23 @@
-vim.cmd.colorscheme("retrobox")
-vim.cmd("TransparentDisable")
+local env_file = ".env"
+
+-- Read .env
+for line in io.lines(env_file) do
+  local key, value = line:match("^([%w_]+)%s*=%s*(.+)$")
+  if key and value then
+    vim.env[key] = value
+  end
+end
+
+local colorscheme = vim.env.COLORSCHEME or "retrobox"
+local transparent = vim.env.TRANSPARENT or "true"
+
+vim.cmd.colorscheme(colorscheme)
+if transparent == "true" then
+  vim.cmd("TransparentEnable")
+else
+  vim.cmd("TransparentDisable")
+end
+
 vim.cmd [[
   highlight Comment gui=italic cterm=italic
   highlight Keyword gui=italic cterm=italic

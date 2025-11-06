96d380e1900503ed1dee24e40a4a4b9edafda6bd
anth <eldiegoemanuel@gmail.com>
2025-11-06 15:07:58 -0500

    feat(toggle term): multi-terminal tabs

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,25 +3,87 @@ return {
   version = "*",
   lazy = false,
   config = function()
-    require("toggleterm").setup{
-      size = 60,
+    local toggleterm = require("toggleterm")
+    toggleterm.setup{
+      size = 20,
       hide_numbers = true,
       shade_terminals = true,
       start_in_insert = true,
       direction = "float",
       float_opts = {
         border = "curved",
-        width = math.floor(vim.o.columns * 0.9),
-        height = math.floor(vim.o.lines * 0.9),
+        width = function() return math.floor(vim.o.columns * 0.8) end,
+        height = function() return math.floor(vim.o.lines * 0.8) end,
         winblend = 0,
       },
     }
 
-    local Terminal  = require('toggleterm.terminal').Terminal
-    local float_term = Terminal:new({ direction = "float" })
+    local Terminal = require('toggleterm.terminal').Terminal
+    local terminals = {}  -- Aquí vamos a guardar nuestras terminales
+    local current_index = -1
 
-    vim.keymap.set({ "n", "t" }, "<C-_>", function()
-      float_term:toggle()
-    end, { noremap = true, silent = true })
+    local function handle_incremental(new_current_index)
+      if new_current_index > #terminals then
+        return 1
+      elseif new_current_index < 1 then
+        return #terminals
+      else
+        return new_current_index
+      end
+    end
+
+    local function next_terminal(incremental)
+      if #terminals == 0 then
+        return
+      end
+      local previus_index = current_index
+      current_index = handle_incremental(current_index + incremental)
+      if previus_index ~= current_index and previus_index ~= -1 then
+        terminals[previus_index]:close()
+      end
+      terminals[current_index]:open()
+    end
+
+    local function new_terminal()
+      local term = Terminal:new({ direction = "float" })
+      table.insert(terminals, term)
+      current_index = #terminals
+      next_terminal(0)
+    end
+
+
+    local function toggle_last_terminal()
+      if #terminals > 0 then
+        terminals[#terminals]:toggle()
+      else
+        new_terminal()
+      end
+    end
+
+    -- Keymaps
+    local opts = { noremap = true, silent = true }
+
+    -- Toggle la última terminal
+    vim.keymap.set("n", "<leader>tt", toggle_last_terminal, opts)
+
+    -- Crear nueva terminal
+    vim.keymap.set("n", "<leader>tn", new_terminal, opts)
+
+    -- Moverse entre terminales (izquierda y derecha)
+    vim.keymap.set("n", "<leader>th", function() next_terminal(-1) end, opts)
+    vim.keymap.set("n", "<leader>tl", function() next_terminal(1) end, opts)
+
+    for i = 1,9 do
+      vim.keymap.set("n", "<leader>t" .. i, function()
+        local difference = i - current_index
+        if difference == 0 then
+          toggle_last_terminal()
+        elseif difference > 0 then
+          next_terminal(difference)
+        else
+          next_terminal(difference)
+        end
+      end, opts)
+    end
   end
 }

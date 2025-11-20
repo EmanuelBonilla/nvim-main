166ea1d8ebfd5e7365a7933da2510f3465aaa9b2
anth <eldiegoemanuel@gmail.com>
2025-11-20 18:26:42 -0500

    feat(lualine): include folder name and relative path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,28 +1,37 @@
-return { 
+return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   lazy = false,
   priority = 1000,
   config = function()
     require("lualine").setup({
-      options = { 
+      options = {
         theme = "auto",
         globalstatus = true,
       },
       sections = {
         lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = { "filename" },
+        lualine_b = { {
+          function()
+            return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+          end,
+          icon = "",
+        } },
+        lualine_c = { { "filename", path = 1 }, },
         lualine_x = {
+          "branch",
           "encoding",
           "fileformat",
           "filetype",
           {
-            function ()
+            function()
               return os.date("%H:%M")
             end,
             icon = "",
-          }
+          },
+          function()
+            return "󰃰 " .. os.date("%y:%m:%d")
+          end,
         },
         lualine_y = { "progress" },
         lualine_z = { "location" },

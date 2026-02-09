df4701fbaa48cc61d7b36abfd6fed6b29b46b8d0
anth <eldiegoemanuel@gmail.com>
2026-02-09 12:45:48 -0500

    feat(cord): update text to show for avoid problems with organizations

--- lua/plugins/cord.lua
+++ lua/plugins/cord.lua
@@ -7,14 +7,24 @@ return {
       editor = {
         image = nil,
         client = 'neovim',
-        tooltip = 'Teto Coder Sigma',
+        tooltip = 'Trash Code',
       },
       idle = {
         enabled = true,
         timeout = 180000, -- 3 minutes
         details = 'idle sybau zZzZzZzZz💤💤',
+      },
+      text = {
+        workspace = function(opts)
+          return "idk 🐢"
+        end,
+        file_browser = "ls -la",
+        editing = function(opts)
+          local fullpath = vim.api.nvim_buf_get_name(0)
+          local dir = vim.fn.fnamemodify(fullpath, ":h:t")
+          return "farming on " .. dir .. "/" .. opts.filename
+        end
       }
-
     }
   end
 }

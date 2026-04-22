1a80b9c1b6705f0081901c956b4539242590fa20
anth <eldiegoemanuel@gmail.com>
2026-04-22 05:24:05 -0500

    chore(flake): format and on execute enter to nvim

--- flake.nix
+++ flake.nix
@@ -1,19 +1,23 @@
 {
   description = "Nvim Flake for develop";
   inputs.nixpkgs.url = "github:NixOs/nixpkgs/nixos-25.05";
-  outputs = {self, nixpkgs}:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in
-  {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        bashInteractive
-        lua-language-server
-        gcc
-        nodejs
-        fzf
-      ];
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          bashInteractive
+          lua-language-server
+          gcc
+          nodejs
+          fzf
+        ];
+        shellHook = ''
+          nvim
+        '';
+      };
     };
-  };
 }

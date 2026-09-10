5861777bc99f35cb93acb8e3866800ea1aed4439
Anthe <eldiegoemanuel@gmail.com>
2026-09-10 00:47:27 -0500

    feat(lsp): csharp

--- lua/lsps/csharp.lua
+++ lua/lsps/csharp.lua
@@ -1,7 +1,14 @@
--- require omnisharp-roslyn
-vim.lsp.config["omnisharp"] = {
-  cmd = { "OmniSharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
-  filetypes = { "cs", "vb" },
+-- require roslyn-ls
+vim.lsp.config["roslyn_ls"] = {
+  cmd = {
+    "Microsoft.CodeAnalysis.LanguageServer",
+    "--logLevel",
+    "Information",
+    "--extensionLogDirectory",
+    vim.fs.joinpath(vim.uv.os_tmpdir(), "roslyn_ls/logs"),
+    "--stdio",
+  },
+  filetypes = { "cs", "razor", "cshtml", "vb" },
   root_markers = {
     function(name, path)
       return name:match("%.sln$") ~= nil or name:match("%.csproj$") ~= nil
@@ -9,14 +16,16 @@ vim.lsp.config["omnisharp"] = {
     ".git",
   },
   settings = {
-    FormattingOptions = {
-      EnableEditorConfigSupport = true,
-      OrganizeImports = true,
+    ["csharp|formatting"] = {
+      dotnet_organize_imports_on_format = true,
     },
-    RoslynExtensionsOptions = {
-      EnableAnalyzersSupport = true,
-      EnableImportCompletion = true,
+    ["csharp|completion"] = {
+      dotnet_show_completion_items_from_unimported_namespaces = true,
+    },
+    ["csharp|background_analysis"] = {
+      dotnet_analyzer_diagnostics_scope = "fullSolution",
+      dotnet_compiler_diagnostics_scope = "fullSolution",
     },
   },
 }
-vim.lsp.enable("omnisharp")
+vim.lsp.enable("roslyn_ls")
